1d14d948acadb4aa9b60b91a7d39740d55ede414
Abhishek Karadbhuje <abhishek.karadbhuje@wisdmlabs.com>
2020-06-09 19:10:16 +0530

    Remove the changes of v38 update

--- build/src/js/vendor/jspdf.js
+++ build/src/js/vendor/jspdf.js
@@ -1,6 +1,5 @@
-! function(t, e) {
-    "object" == typeof exports && "undefined" != typeof module ? module.exports = e() : "function" == typeof define && define.amd ? define(e) : t.jspdf = e()
-}(this, function() {
+define('report_elucidsitereport/jspdf', [], function() {
+    
     "use strict";
     var t = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function(t) {
             return typeof t
