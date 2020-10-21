09e5c8162ce5b4d3b44483e83755fc5bc22ab1d8
Abhishek Karadbhuje <abhishek.karadbhuje@wisdmlabs.com>
2020-10-21 15:16:50 +0530

    Updated version after quick fix for content bank

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_edwiserreports';
 $plugin->release = '1.0.0';
-$plugin->version = 2020100700;
+$plugin->version = 2020102100;
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_STABLE;
