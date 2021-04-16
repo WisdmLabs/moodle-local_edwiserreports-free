6c3899ad54e2235da6bea94bf244a668b2813fc6
Yogesh <yogesh.shirsath@wisdmlabs.com>
2021-04-16 18:19:46 +0530

    Updated version to v1.0.2 and updated CHANGES.txt

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_edwiserreports';
-$plugin->release   = '1.0.1';
-$plugin->version   = 2021040901;
+$plugin->release   = '1.0.2';
+$plugin->version   = 2021041600;
 $plugin->requires  = 2018120300;
 $plugin->maturity  = MATURITY_STABLE;
