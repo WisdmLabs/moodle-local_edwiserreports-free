38db4af7ed856aa3bd955ec8799ccedb2ee6ba2e
Yogesh Shirsath <yogesh.shirsath@wisdmlabs.com>
2022-06-13 19:19:55 +0530

    Updated version for v1.3.0

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_edwiserreports';
-$plugin->release   = '1.2.0';
-$plugin->version   = 2022032000;
+$plugin->release   = '1.3.0';
+$plugin->version   = 2022043000;
 $plugin->requires  = 2018120300;
 $plugin->maturity  = MATURITY_STABLE;
